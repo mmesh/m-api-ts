da0e374af6d167ec563255d07cdc548b099e8f46
x6a <x6a@7n.io>
2024-01-03 03:44:44 +0000

    [wip] auto-commit v0.0.1093

--- mmesh/protobuf/common/v1/blob/blob.pb.ts
+++ mmesh/protobuf/common/v1/blob/blob.pb.ts
@@ -6,3 +6,11 @@
 export type Blob = {
   data?: Uint8Array
 }
+
+export type Chunk = {
+  sizeInBytes?: string
+  chunkChecksum?: Uint8Array
+  data?: Uint8Array
+  isLastChunk?: boolean
+  payloadChecksum?: Uint8Array
+}
--- mmesh/protobuf/resources/v1/nstore/hsecdb/hsecdb.pb.ts
+++ mmesh/protobuf/resources/v1/nstore/hsecdb/hsecdb.pb.ts
@@ -4,6 +4,7 @@
 * This file is a generated Typescript file for GRPC Gateway, DO NOT MODIFY
 */
 
+import * as BlobBlob from "../../../../common/v1/blob/blob.pb"
 import * as HsecReport from "../../hsec/report.pb"
 import * as NstoreNstore from "../nstore.pb"
 export type HostSecurityReportRequest = {
@@ -16,5 +17,6 @@ export type HostSecurityReportResponse = {
   nodeID?: string
   queryID?: string
   report?: HsecReport.Report
+  reportChunk?: BlobBlob.Chunk
   timestamp?: string
 }
